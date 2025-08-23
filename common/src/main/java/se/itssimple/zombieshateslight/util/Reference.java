f903e64c706add1cf898361287bcc6490351b0ef
NoLifeKing <github-things@nolifeking85.tv>
2025-08-23 17:05:14 +0200

    wip: Trying to get Obsidianweave to work properly..

--- common/src/main/java/se/itssimple/zombieshateslight/util/Reference.java
+++ common/src/main/java/se/itssimple/zombieshateslight/util/Reference.java
@@ -3,6 +3,6 @@ package se.itssimple.zombieshateslight.util;
 public class Reference {
 	public static final String MOD_ID = "zombieshateslight";
 	public static final String NAME = "Zombies Hates Light";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.2.0";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
