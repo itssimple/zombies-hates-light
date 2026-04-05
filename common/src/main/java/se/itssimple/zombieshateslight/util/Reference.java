e0c8e243c8680f5f4dcfc3e34581ad9c4a441e80
NoLifeKing <github-things@nolifeking85.tv>
2026-04-05 14:30:39 +0200

    feat: Mob config for 1.20.1, Forge is broken due to Obsidian Weave being bad.
    ref https://github.com/itssimple/.mod-issue-tracker/issues/7

--- common/src/main/java/se/itssimple/zombieshateslight/util/Reference.java
+++ common/src/main/java/se/itssimple/zombieshateslight/util/Reference.java
@@ -3,6 +3,6 @@ package se.itssimple.zombieshateslight.util;
 public class Reference {
 	public static final String MOD_ID = "zombieshateslight";
 	public static final String NAME = "Zombies Hates Light";
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.3.0";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
